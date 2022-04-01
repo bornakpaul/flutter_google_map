dc31a2a1f9ddddf4aca61e8a4eaaf2342082d1d2
Bornak <bornakpaul@gmail.com>
2022-04-01 10:44:03 +0530

    debounce added

--- lib/ui/search_screen.dart
+++ lib/ui/search_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_place/google_place.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -10,6 +13,27 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   final _startSearchController = TextEditingController();
   final _endSearchController = TextEditingController();
+
+  late GooglePlace googlePlace;
+  List<AutocompletePrediction> predictions = [];
+  Timer? debounce;
+
+  @override
+  void initState() {
+    super.initState();
+    String apikey = "AIzaSyA60flZWx5tZvTgyDF_8jXTqlii04GvZps";
+    googlePlace = GooglePlace(apikey);
+  }
+
+  void autoCompleteSearch(String value) async {
+    var result = await googlePlace.autocomplete.get(value);
+    if (result != null && result.predictions != null && mounted) {
+      setState(() {
+        predictions = result.predictions!;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,21 +49,30 @@ class _SearchScreenState extends State<SearchScreen> {
             TextField(
               controller: _startSearchController,
               autofocus: false,
-              showCursor: false,
+              style: const TextStyle(fontSize: 18, color: Colors.white),
               decoration: InputDecoration(
                 hintText: 'Starting Point',
                 hintStyle: const TextStyle(
                     fontWeight: FontWeight.w500,
                     fontSize: 18,
-                    color: Colors.white),
+                    color: Colors.white70),
                 filled: true,
-                fillColor: Color(0xff5584AC),
+                fillColor: const Color(0xff5584AC),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(4.0),
                   gapPadding: 0.0,
                   borderSide: BorderSide.none,
                 ),
               ),
+              cursorColor: Colors.white,
+              onChanged: (value) {
+                if (debounce?.isActive ?? false) debounce!.cancel();
+                debounce = Timer(const Duration(milliseconds: 1000), () {
+                  if (value.isNotEmpty) {
+                    autoCompleteSearch(value);
+                  } else {}
+                });
+              },
             ),
             const SizedBox(
               height: 10,
@@ -47,21 +80,22 @@ class _SearchScreenState extends State<SearchScreen> {
             TextField(
               controller: _endSearchController,
               autofocus: false,
-              showCursor: false,
+              style: const TextStyle(fontSize: 18, color: Colors.white),
               decoration: InputDecoration(
                 hintText: 'Destination Point',
                 hintStyle: const TextStyle(
                     fontWeight: FontWeight.w500,
                     fontSize: 18,
-                    color: Colors.white),
+                    color: Colors.white70),
                 filled: true,
-                fillColor: Color(0xff5584AC),
+                fillColor: const Color(0xff5584AC),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(4.0),
                   gapPadding: 0.0,
                   borderSide: BorderSide.none,
                 ),
               ),
+              cursorColor: Colors.white,
             ),
           ],
         ),
